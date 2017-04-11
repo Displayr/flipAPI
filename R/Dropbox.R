19ca827dc757a83ed3eeb39926af6fd64d9bc2c4
chschan <chschan@gmail.com>
2017-04-11 14:18:15 +1000

    Fix warnings and documentation about updating

--- R/Dropbox.R
+++ R/Dropbox.R
@@ -5,11 +5,13 @@
 #'    named \code{<object>.rds}. This will overwrite existing files in your dropbox with the same name.
 #'    When multiple copies of the same object under different conditions are saved, it may be preferrable to
 #'    manually specify the filename.
-#' @param reexport.seconds Time in seconds after which object will be re-exported. This option works only in Displayr. See \link{UpdateObject} for more details.
+#' @param reexport.seconds Time in seconds after which object will be re-exported. This option works only in Displayr.
+#'    If the document is opened, and the object has not been exported for \code{reexport.seconds}, then the object
+#'    is re-exported. By default this is set to 1 week. Setting a negative value will disable the automatic update.
 #' @importFrom httr PUT add_headers upload_file
 #' @export
 
-ExportToDropbox <- function(object, token, file=NA, reexport.seconds = -1)
+ExportToDropbox <- function(object, token, file=NA, reexport.seconds = 604800)
 {
     if (reexport.seconds > 0)
         message(sprintf("R output expires in %d", round(reexport.seconds)))
@@ -25,10 +27,10 @@ ExportToDropbox <- function(object, token, file=NA, reexport.seconds = -1)
     returnMsg <- "Could not upload object. Check that dropbox token is correct."
     if (!inherits(pp, "try-error") && pp$status_code == 200)
     {
-        cmd1 <- sprintf("TriggerObjectUpdate('%s', <document api key>)", 
-                        as.character(substitute(object)))
+        #cmd1 <- sprintf("TriggerObjectUpdate('%s', <document api key>)", 
+        #                as.character(substitute(object)))
         cmd2 <- sprintf("ImportFromDropbox('%s', <dropbox token>)", file)
-        returnMsg <- paste("Object uploaded to dropbox. To re-import object use:\n   > library(flipAPI)\n   >", cmd1, "\n   >", cmd2, "\n")
+        returnMsg <- paste("Object uploaded to dropbox. To re-import object use:\n   > library(flipAPI)\n   >", cmd2, "\n")
     }
     invisible(file.remove(file))
     returnMsg
