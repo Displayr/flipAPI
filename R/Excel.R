91714032417496468b24f06b82e0556b9a93f9aa
Mathew W. McLean <mathew.w.mclean@gmail.com>
2017-09-27 20:30:21 +1000

    Use ParseUserEnteredTable
    * ParseEnteredData deprecated in
    flipTransformations
    
    Addresses: DS-1471

--- R/Excel.R
+++ R/Excel.R
@@ -5,8 +5,8 @@
 #' @importFrom utils download.file
 #' @importFrom readxl read_xlsx
 #' @importFrom httr GET
-#' @importFrom flipTransformations ParseEnteredData ParseAsDataFrame
-#' @param want.data.frame Whether to return a data frame instead of a matrix or vector. Note that unlike \link[flipTransformations]{ParseEnteredData}, the default is \code{TRUE}, because imported data files are expected to be larger than cut-and-paste data.
+#' @importFrom flipTransformations ParseUserEnteredTable ParseAsDataFrame
+#' @param want.data.frame Whether to return a data frame instead of a matrix or vector.
 #' @param want.factors Whether a text variable should be converted to a factor in a data frame.
 #' @param want.col.names Whether to interpret the first row as column names in a data frame.
 #' @param want.row.names Whether to interpret the first col as row names in a data frame.
@@ -17,13 +17,13 @@ DownloadXLSX <- function(url, sheet = 1, want.data.frame = FALSE, want.factors =
 {
     if (!(grepl("http|^ftp", url)))
         stop("URL should begin with 'http', 'https' or 'ftp'")
-        
+
     # Convert link from a re-direct to direct URL
     if (grepl("dropbox", url))
         url <- gsub("dl=0$", "dl=1", url)
     if (grepl("google", url))
         url <- gsub("edit[#?].*", "export?format=xlsx", url)
-    
+
     tmp.name <- tempfile(tmpdir=".")
     tmp.file <- try(download.file(url, destfile=tmp.name, mode="wb"))
     if (inherits(tmp.file, "try-error"))
@@ -36,26 +36,26 @@ DownloadXLSX <- function(url, sheet = 1, want.data.frame = FALSE, want.factors =
         url <- gsub("redir?", "download", retry$url) # one-drive
         unlink(tmp.name)
         tmp.name <- tempfile(tmpdir=".")
-        
+
         tmp.file <- try(download.file(url, destfile=tmp.name, mode="wb"))
         if (inherits(tmp.file, "try-error"))
             stop("Could not download file from ", url, "\n")
-        
+
         res <- try(read_xlsx(tmp.name, sheet=sheet, col_names=(want.data.frame && want.col.names)))
         if (inherits(res, "try-error"))
             stop("File is not a valid XLSX file\n")
     }
     unlink(tmp.name)
-    
+
     if (!want.data.frame)
     {
         res <- as.matrix(res)
         res[which(is.na(res))] <- ""
         dimnames(res) <- NULL
-        return(ParseEnteredData(res, want.data.frame = FALSE))
+        return(ParseUserEnteredTable(res, want.data.frame = FALSE))
     }
-    
-    res.formatted <- ParseAsDataFrame(res, warn=T, want.factors=want.factors, want.col.names=FALSE, 
+
+    res.formatted <- ParseAsDataFrame(res, warn=T, want.factors=want.factors, want.col.names=FALSE,
                                       want.row.names=want.row.names, us.format=us.format)
     return(res.formatted)
 }
