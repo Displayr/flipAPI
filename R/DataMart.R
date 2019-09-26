880a6c0254a242240523625c42badd39f8c3b6a4
Jeremy Tay <jeremy.tay@displayr.com>
2019-09-26 10:56:18 +1000

    RS-3782: Fix documentation and logic. SET URLS TO TEST.DISPLAYR FOR TESTING

--- R/DataMart.R
+++ R/DataMart.R
@@ -1,21 +1,20 @@
-#' NOTE: These functions are currently only supported for use through DisplayR. The API cannot be accessed externally. 
-
-url <- "http://localhost/API/DataMart" # JUST USE APP.DISPLAYR.COM? 
-companySecret <- "141e27d4-19e1-47cd-86fc-9604a29141cf"
-
-companySecret <- ifelse(exists("companySecret"), companySecret, "")
-if (companySecret == "")
-    message("Warning: This package is currently only supported for use through DisplayR. You will not be able to run any of these functions.")
-
-#' Check if a file exists in the Data Mart
+#' Check if a file exists
+#' 
+#' Check whether a file of a given name exists in the Data Mart.
+#' 
 #' @param filename character string. Name of the file to search for
-#' @return Returns true if the file exists.
+#' 
+#' @return TRUE if the file exists, otherwise FALSE
+#' 
 #' @importFrom httr HEAD add_headers
+#' 
 #' @export
 QFileExists <- function(filename) 
 {
-    res <- try(HEAD(paste0(url, "?filename=", filename), 
-                    config=add_headers("x-q-company-secret" = companySecret)))
+    companySecret <- ifelse(exists("companySecret"), companySecret, "")
+    res <- try(HEAD(paste0("https://test.displayr.com/api/DataMart?filename=", filename), 
+                    config=add_headers("X-Q-Company-Secret" = companySecret)))
+    
     if (is.null(res$status_code) || res$status_code != 200)
     {
         msg <- ifelse(res$status_code == 404 , "File not found.", "Could not connect to Data Mart.")
@@ -28,17 +27,26 @@ QFileExists <- function(filename)
     }
 }
 
-#' Opens a file in the data mart for reading or writing
+#' Opens a Connection
+#' 
+#' Opens a connection for either reading OR writing to a file of a given name.
+#' In the reading case, it opens a stream to the file in the Data Mart. 
+#' In the writing case, it opens a temporary file for writing to and uploads this to the Data Mart on close.
+#' Note that writing to a file which already exists will overwrite that file's contents.
+#' 
 #' @param filename character string. Name of file to be opened
 #' @param open character string. A description of how to open the connection
 #' @param blocking logical. Whether or not the connection blocks can be specified
-#' @param encoding The name of the encoding to be assumed.
+#' @param encoding character string. The name of the encoding to be assumed.
 #' @param raw logical. Whether this connection should be treated as a byte stream
 #' @param method character string. See documentation for connections
-#' @return Returns a connection to a data blob
-#' @importFrom curl curl handle
-#' @importFrom httr POST upload_file add_headers
-#' @inportFrom mime file_type
+#' 
+#' @return A curl connection (read) or a file connection (write)
+#' 
+#' @importFrom curl curl new_handle
+#' @importFrom httr POST upload_file add_headers handle 
+#' @importFrom tools file_ext
+#' 
 #' @export
 QFileOpen <- function(filename, open = "r", blocking = TRUE, 
                       encoding = getOption("encoding"), raw = FALSE, 
@@ -47,11 +55,12 @@ QFileOpen <- function(filename, open = "r", blocking = TRUE,
     mode <- tolower(open)
     if (mode == "r" || mode == "rb") 
     {
+        companySecret <- ifelse(exists("companySecret"), companySecret, "")
         h <- new_handle()
         handle_setheaders(h,
             "X-Q-Company-Secret" = companySecret
         )
-        conn <- try(curl(paste0(url,"/DataMart?filename=", filename),
+        conn <- try(curl(paste0("https://test.displayr.com/api/DataMart?filename=", filename),
                         open = mode,
                         handle = h), 
                     silent = TRUE)
@@ -68,57 +77,74 @@ QFileOpen <- function(filename, open = "r", blocking = TRUE,
         return (conn)
     } else if (mode == "w" || mode == "wb") 
     {
+        if (!exists('companySecret'))
+            stop("Could not connect to Data Mart.")
+        
         tmpfile <- paste0(tempfile(), ".", file_ext(filename))
         conn <- file(tmpfile, mode, blocking, encoding, raw, method)
         class(conn) = append("qpostconn", class(conn))
-        
-        #do error checking here? e.g. HTTP 400 
         
         # store attributes for later access
         attr(conn, "tmpfile") <- tmpfile
         attr(conn, "filename") <- filename
 
         return (conn)
+    } else 
+    {
+        stop("Invalid mode - please use either 'r', 'rb','w' or 'wb'.")
     }
-    warning("Invalid mode - please use either 'r', 'rb','w' or 'wb'.")
 }
 
-# Overload so that we post to the Data Mart on close
+#' Closes a QFileOpen connection
+#' 
+#' This is an overload for close.connection which writes the file contents 
+#' of a connection opened using QFileOpen to the Data Mart.
+#' 
+#' @param conn connection object of class 'qpostconn'. Connection opened with QFileOpen
+#' 
+#' @importFrom httr POST add_headers upload_file
+#' 
+#' @export
 close.qpostconn = function(conn) 
 {
     close.connection(conn)
     filename <- attr(conn, "filename")
     tmpfile <- attr(conn, "tmpfile")
-    
-    res <- try(POST(paste0(url, "?filename=", filename),
+    on.exit(if(file.exists(tmpfile)) file.remove(tmpfile))
+
+    companySecret <- ifelse(exists("companySecret"), companySecret, "")    
+    res <- try(POST(paste0("https://test.displayr.com/api/DataMart?filename=", filename),
                 config = add_headers("Content-Type" = mime::guess_type(filename),
                                      "X-Q-Company-Secret" = companySecret),
                 encode = "raw",
                 body = upload_file(tmpfile)))
     
-    
-    
     if (inherits(res, "try-error") || res$status_code != 200)
-        warning("Could not write to data mart.")
+        stop("Could not write to data mart.")
     else 
         message("File was written successfully.")
-    
-    invisible(file.remove(tmpfile))
 }
 
-#' Loads rds data from the data mart into an object
+#' Loads an object
+#' 
+#' Loads an *.rds file from the data mart and converts this to an R object.
+#' 
 #' @param filename character string. Name of the file to be opened from the Data Mart
-#' @return Returns an R object
+#' 
+#' @return An R object
+#' 
 #' @importFrom httr GET add_headers write_disk
+#' @importFrom tools file_ext
+#' 
 #' @export
 QLoadData <- function(filename) 
 {
     if (file_ext(filename) != "rds") 
-        stop("Can only load data from rds objects.")
+        stop("Can only load data from *.rds objects.")
     
     tmpfile <- tempfile()
-
-    req <- try(GET(paste0(url, "?filename=", filename),
+    companySecret <- ifelse(exists("companySecret"), companySecret, "")
+    req <- try(GET(paste0("https://test.displayr.com/api/DataMart?filename=", filename),
                config=add_headers("X-Q-Company-Secret" = companySecret),
                write_disk(tmpfile, overwrite = TRUE)))
     
@@ -127,17 +153,29 @@ QLoadData <- function(filename)
         msg <- ifelse(req$status_code == 404, "File not found.", "Could not connect to Data Mart.")
         stop(msg)
     }
-        
-    obj <- readRDS(tmpfile)
-    invisible(file.remove(tmpfile))
-    return (obj)
-    
+    if (file.exists(tmpfile)) 
+    {
+        obj <- readRDS(tmpfile) 
+        file.remove(tmpfile)
+        return (obj)
+    }
+    stop("Could not read from file.")
 }
 
-#' Saves an object into an rds file blob
+#' Save an object
+#' 
+#' Saves an object to the Data Mart as an *.rds file of the given name.
+#' If the name is given an extension, this must be '.rds'. If there is no
+#' extension specified, this defaults to '.rds'.
+#' 
 #' @param object object. The object to be uploaded
 #' @param filename character string. Name of the file to be written to
+#' 
+#' @return 
+#' 
 #' @importFrom httr POST add_headers upload_file
+#' @importFrom tools file_ext
+#' 
 #' @export 
 QSaveData <- function(object, filename)
 {
@@ -145,26 +183,26 @@ QSaveData <- function(object, filename)
         filename <- append(filename, ".rds")
     
     if (file_ext(filename) != "rds")
-        stop("File-type must be .rds")
+        stop("File must be of type *.rds")
     
     tmpfile <- tempfile()
     saveRDS(object, tmpfile)
+    on.exit(if(file.exists(tmpfile)) file.remove(tmpfile))
     
-    res <- try(POST(paste0(url, "?filename=", filename), 
+    companySecret <- ifelse(exists("companySecret"), companySecret, "")
+    res <- try(POST(paste0("https://test.displayr.com/api/DataMart?filename=", filename), 
                 config = add_headers("Content-Type" = "application/x-gzip", # default is gzip for saveRDS
                                      "X-Q-Company-Secret" = companySecret),
                 encode = "raw",
                 body = upload_file(tmpfile)))
     
-    invisible(file.remove(tmpfile))
-    
     if (inherits(res, "try-error") || res$status_code != 200)
         stop("Could not save file")
     
-    message(paste("Object uploaded to Data Mart To re-import object use:",
+    msg <- paste("Object uploaded to Data Mart To re-import object use:",
                   "   > library(flipAPI)",
            paste0("   > QLoadData('", filename, "')"),
-           sep = "\n"))
+           sep = "\n")
+    message(msg)
 }
-
 
