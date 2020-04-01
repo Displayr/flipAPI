f161d007587e65912a432b4211c9459c61479cf6
oliverbock <oliver@g7.org>
2020-04-01 15:22:58 +1100

    Still hoping to guess the right region to get these tests to pass.

--- tests/testthat/helper-datamart.R
+++ tests/testthat/helper-datamart.R
@@ -1,3 +1,3 @@
 companySecret <- get0("companySecret", ifnotfound = Sys.getenv("companySecret"))
 clientId <- "123456" # This could be anything - we are just using this for metadata
-region <- "test"
+region <- "master"
