59d0c78204a1b4fbeedfaa85c75a6db56dbb2e40
flipDevTools <opensource@displayr.com>
2021-04-12 11:24:16 +1000

    Try to fix travis build; DS-3269

--- tests/testthat/helper-datamart.R
+++ tests/testthat/helper-datamart.R
@@ -1,3 +1,4 @@
 companySecret <- get0("companySecret", ifnotfound = Sys.getenv("companySecret"))
+assign("companySecret", companySecret, envir = .GlobalEnv)
 clientId <- "123456" # This could be anything - we are just using this for metadata
 region <- "master"
