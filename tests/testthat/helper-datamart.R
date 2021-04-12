e0eb15693af070c395afb2e9e52114db32007905
flipDevTools <opensource@displayr.com>
2021-04-12 12:22:15 +1000

    Fix travis build issue finding env vars; DS-3269

--- tests/testthat/helper-datamart.R
+++ tests/testthat/helper-datamart.R
@@ -1,4 +1,6 @@
 companySecret <- get0("companySecret", ifnotfound = Sys.getenv("companySecret"))
 assign("companySecret", companySecret, envir = .GlobalEnv)
 clientId <- "123456" # This could be anything - we are just using this for metadata
+assign("clientId", clientId, envir = .GlobalEnv)
 region <- "master"
+assign("region", region, envir = .GlobalEnv)
