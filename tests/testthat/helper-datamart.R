8fefa8e6ef614a896c4c0d324f3076e75ae7671e
Mathew W. McLean <mathew.w.mclean@gmail.com>
2023-04-21 10:54:55 +1000

    DS-4538: Try to fix unit test by updating env. vars.

--- tests/testthat/helper-datamart.R
+++ tests/testthat/helper-datamart.R
@@ -1,6 +1,6 @@
 companySecret <- get0("companySecret", ifnotfound = Sys.getenv("companySecret"))
 assign("companySecret", companySecret, envir = .GlobalEnv)
-clientId <- "123456" # This could be anything - we are just using this for metadata
+clientId <- "-1027046" # This could be anything - we are just using this for metadata
 assign("clientId", clientId, envir = .GlobalEnv)
-region <- "master"
+region <- "app"
 assign("region", region, envir = .GlobalEnv)
