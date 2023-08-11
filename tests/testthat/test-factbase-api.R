445fe3ea2ee4988853c6afae2f9346c2210244a4
Oliver Bock <oliver.bock@displayr.com>
2023-08-11 11:16:44 +1000

    untracked: Old merge conflict

--- tests/testthat/test-factbase-api.R
+++ tests/testthat/test-factbase-api.R
@@ -91,11 +91,7 @@ test_that("UploadMetricToFactbase() can handle pre-aggregated data and use the `
 "dimensionType": "period_type_in_table_name",
 "valueType": "datetime",
 "unique": false,
-<<<<<<< HEAD
-"valueForTheseObservations": "Day" 
-=======
 "valueForTheseObservations": "Day",
->>>>>>> 6f66735 (untracked: Not sure how this got through)
 "periodType": "day" 
 },
 {
