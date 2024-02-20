c54ab1397ccbc8c3907930b78039a9dffa452635
Aiden Gu <aiden.gu@displayr.com>
2024-02-21 10:53:33 +1100

    Point api to master if region specifies master

--- tests/testthat/helper-datamart.R
+++ tests/testthat/helper-datamart.R
@@ -2,5 +2,5 @@ companySecret <- get0("companySecret", ifnotfound = Sys.getenv("companySecret"))
 assign("companySecret", companySecret, envir = .GlobalEnv)
 clientId <- "-1027046" # This could be anything - we are just using this for metadata
 assign("clientId", clientId, envir = .GlobalEnv)
-region <- "app"
+region <- "master"
 assign("region", region, envir = .GlobalEnv)
